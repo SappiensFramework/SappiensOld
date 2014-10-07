b323c700e7faf9acb06d1ed73c9bff8ce7b941e6
Pozzebon <vpozzebon@gmail.com>
2014-10-07 14:48:40 -0400

    Implementado o Template.php e o index.php no /dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,7 +1,6 @@
 <?php
 
 require '../Config.php';
-$padrao = new \Zion\Layout\Padrao();
 $html   = new \Zion\Layout\Html();
 
 try {
@@ -15,32 +14,8 @@ try {
 
 // Define o nome do módulo e o nome amigável ao usuário
 define('DEFAULT_MODULO_NOME', 'Dashboard');
+define('DEFAULT_MODULO_URL', 'dashboard');
 
-echo $padrao->topo();
-echo $template->getCabecalho();
-
-echo $html->abreTagAberta('body', array('class' => 'theme-asphalt main-menu-animated'));
-echo $html->abreTagAberta('div', array('id' => 'main-wrapper'));
-
-echo $template->getBarraSuperior();	
-echo $template->getMenuLateral();
-
-// start: content-wrapper
-echo $html->abreTagAberta('div', array('id' => 'content-wrapper'));
-// end: content-wrapper
-echo $html->fechaTag('div');
-
-// start: main-menu-bg
-echo $html->abreTagAberta('div', array('id' => 'main-menu-bg'));
-// end: main-menu-bg
-echo $html->fechaTag('div');
-
-// end: main-navbar
-echo $html->fechaTag('div');
-// end: main-wrapper
-echo $html->fechaTag('div');
-
-echo $template->getRodape();
-echo $padrao->rodape();
+echo $template->getTemplate();
 
 ?>
