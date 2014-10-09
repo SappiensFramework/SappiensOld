ef9d55cabce1cf679732413caa6ddebe76b8ef96
Feliphe <feliphe@braconsultoria.com.br>
2014-10-09 09:04:22 -0400

    Atualização nas configs.

--- Config.php
+++ Config.php
@@ -75,6 +75,6 @@ require_once SIS_FM_BASE . 'Lib/Zion/ClassLoader/Loader.class.php';
 
 (new \Zion\ClassLoader\Loader())
         ->setNamEspaces('Zion','C:/xampp/htdocs/Zion/Lib') //NameSpace do Framework
-        ->setNamEspaces(SIS_ID_NAMESPACE_PROJETO,SIS_NAMESPACE_PROJETO) //NameSpace do Projeto
+        ->setNamEspaces(SIS_ID_NAMESPACE_PROJETO, SIS_NAMESPACE_PROJETO) //NameSpace do Projeto
         ->setSufixos(array('', '.vo', '.class', '.interface')) //Sufixos em Geral
         ->inicio();
