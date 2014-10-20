fe8c29d155fd49f6b99552bb3331feff9878e97a
Pozzebon <vpozzebon@gmail.com>
2014-10-20 10:03:58 -0300

    Subindo form do selecionar cliente

---
+++ includes/SelecionarClienteForm.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Sappiens\includes;
+
+class SelecionarClienteForm
+{
+
+    /**
+     * 
+     * @return Form
+     */
+    public function getFormModulo()
+    {
+        $form = new \Pixel\Form\Form();
+/*
+        $form->config('Form1', 'GET')
+                ->setNovalidate(true)
+                //->setHeader('Selecionar cliente')
+                ->setClassCss('navbar-form pull-left')
+                ->setTarget('_blank')
+                ->setAction('recebe.php');
+*/
+        $campos[] = $form->suggest('cidade', 'Cidades', false)
+                ->setTabela('cidade')
+                ->setCampoDesc('CidadeNome')
+                ->setPlaceHolder('Pesquisar...')
+                ->setEmColunaDeTamanho(12);
+/*
+          $campos[] = $form->botaoSubmit('enviar', 'Continuar')
+          ->setClassCss('btn btn-primary'); 
+          
+          $campos[] = $form->botaoReset('limpar', 'Limpar')
+          ->setClassCss('btn btn-default'); 
+*/
+        return $form->processarForm($campos);
+    }
+
+}
