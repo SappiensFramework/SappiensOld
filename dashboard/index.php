bf71055b464d8d66624e8c162a83a2dca9f34876
Pozzebon <vpozzebon@gmail.com>
2014-10-25 17:52:08 -0300

    Implementado modelo de panel e tabs

--- dashboard/index.php
+++ dashboard/index.php
@@ -7,8 +7,39 @@ try {
 	$html     = new \Zion\Layout\Html();
 	$template = new \Pixel\Template\Template();
 
+    $tabArray = array(
+        array('tabId' => 1,
+              'tabActive' => 'active',
+              'tabTitle' => 'Titulo da tab 1', 
+              'tabContent' => 'Conteudo da tab 1'
+              ),
+        array('tabId' => 2,
+              'tabActive' => '',
+              'tabTitle' => 'Titulo da tab 2', 
+              'tabContent' => 'Conteudo da tab 2'
+              ),
+        array('tabId' => 3,
+              'tabActive' => '',
+              'tabTitle' => 'Titulo da tab 3', 
+              'tabContent' => 'Conteudo da tab 3'
+              ),
+        array('tabId' => 4,
+              'tabActive' => '',
+              'tabTitle' => 'Titulo da tab 4', 
+              'tabContent' => 'Conteudo da tab 4'
+              ),
+        array('tabId' => 5,
+              'tabActive' => '',
+              'tabTitle' => 'Titulo da tab 5', 
+              'tabContent' => 'Conteudo da tab 5'
+              )                    
+        );
+
+    $tab = $template->getTab('tabWelcome', array('classCss' => 'col-sm-6'), $tabArray);
+    $panel = $template->getPanel('box-welcome', 'Bem-vindo', $tab, ['titleVisible' => true, 'startVisible' => true, 'iconTitle' => 'fa fa-filter']);
+
 	$template->setConteudoHeader();
-	$template->setConteudoMain('Conteúdo do Dashboard');
+	$template->setConteudoMain($panel);
 	$template->setConteudoScripts();
 	$template->setConteudoFooter();
 
