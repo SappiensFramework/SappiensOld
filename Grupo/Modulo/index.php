b3d0e94df3b23f8424fe143c1fb16765385c806d
Pablo Vanni <pablovanni@gmail.com>
2014-10-30 16:30:19 -0300

    Finalizações do Crud

--- Grupo/Modulo/index.php
+++ Grupo/Modulo/index.php
@@ -1,7 +1,10 @@
 <?php
+
 require '../../Config.php';
 
+define('GRUPO', 'Grupo');
 define('MODULO', 'Modulo');
-define('DEFAULT_MODULO_NOME', 'Dashboard');
-define('DEFAULT_MODULO_URL', 'dashboard');
-echo (new \Sappiens\Grupo\Modulo\ModuloController())->controle(\filter_input(INPUT_GET, 'acao'));
+define('DEFAULT_MODULO_NOME', 'Módulo');
+define('DEFAULT_MODULO_URL', 'Modulo');
+
+echo (new \Sappiens\Grupo\Modulo\ModuloController())->controle(\filter_input(\INPUT_GET, 'acao'));
