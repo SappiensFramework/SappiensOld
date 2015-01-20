d522390c88df142945f6afa75609cd8b29ce4c67
Feliphe <feliphe@braconsultoria.com.br>
2015-01-20 10:45:36 -0200

    Correção na exibição de erros do sistema.

--- Storage/ArquivoDownload.php
+++ Storage/ArquivoDownload.php
@@ -1,6 +1,6 @@
 <?php
 
-session_start();
+//session_start();
 
 require '../Config.php';
 
@@ -14,53 +14,51 @@ if ($modo != 'download' and $modo != 'ver') {
     exit("Modo de Visualização inválido");
 }
 
-//Buscando mais informações
-$sqlArquivo = "SELECT uploadNomeFisico, uploadNomeOriginal, uploadDataCadastro, uploadMime FROM _upload WHERE uploadCod = " . $uploadCod;
+    try{
 
-try {
-    $dadosArquivo = $con->execLinha($sqlArquivo);
-} catch (Exception $e) {
-    exit($e->getMessage());
-}
-
-$arquivo = SIS_DIR_BASE . 'Storage/' . \str_replace('-', '/', $dadosArquivo['uploadDataCadastro']) . '/' . $dadosArquivo['uploadNomeFisico'];
-
-if (!file_exists($arquivo)) {
-    exit("Arquivo não encontrado");
-}
-
-$extensaoAtual = \strtolower($manipulaArquivo->extenssaoArquivo($dadosArquivo['uploadNomeOriginal']));
-
-if ($modo == 'ver') { //Visualizar icone do arquivo, se for imagem mostrar ela mesma
-    //Extenssões que é possivel ele ver
-    $extensoesVer = ['jpeg', 'gif', 'png'];
-
-    if (\in_array($extensaoAtual, $extensoesVer)) {
-        \header('Content-Type: image/' . $extensaoAtual);
-        \readfile($arquivo);
-    } else {
-        //Caminho para Uma Imagem Padrão
-        \header('Content-Type: image/gif');
-        //\readfile($_SESSION['DirBase'] . 'figuras/icone0.gif');
-    }
-} else { //Download do Arquivo
-    //Atualiza Numero de Dowloads
-    try {
-        $con->executar("UPDATE _upload SET uploadDownloads = (uploadDownloads +1) WHERE uploadCod = $uploadCod");
-    } catch (\Exception $e) {
+        //Buscando mais informações
+        $sqlArquivo = "SELECT uploadNomeFisico, uploadNomeOriginal, uploadDataCadastro, uploadMime FROM _upload WHERE uploadCod = " . $uploadCod;
         
+        $dadosArquivo = $con->execLinha($sqlArquivo);
+        
+        $arquivo = SIS_DIR_BASE . 'Storage/' . \str_replace('-', '/', $dadosArquivo['uploadDataCadastro']) . '/' . $dadosArquivo['uploadNomeFisico'];
+        
+        if (!file_exists($arquivo)) {
+            exit("Arquivo não encontrado");
+        }
+        
+        $extensaoAtual = \strtolower($manipulaArquivo->extenssaoArquivo($dadosArquivo['uploadNomeOriginal']));
+        
+        if ($modo == 'ver') { //Visualizar icone do arquivo, se for imagem mostrar ela mesma
+            //Extenssões que é possivel ele ver
+            $extensoesVer = ['jpeg', 'gif', 'png'];
+        
+            if (\in_array($extensaoAtual, $extensoesVer)) {
+                \header('Content-Type: image/' . $extensaoAtual);
+                \readfile($arquivo);
+            } else {
+                //Caminho para Uma Imagem Padrão
+                \header('Content-Type: image/gif');
+                //\readfile($_SESSION['DirBase'] . 'figuras/icone0.gif');
+            }
+        } else { //Download do Arquivo
+            //Atualiza Numero de Dowloads
+            $con->executar("UPDATE _upload SET uploadDownloads = (uploadDownloads +1) WHERE uploadCod = $uploadCod");
+        
+            \set_time_limit(0);
+        
+            \header("Pragma: public");
+            \header("Expires: 0");
+            \header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+            \header("Cache-Control: private", false);
+            //header("Content-Type:".mime_content_type($arquivo)."");
+            \header("Content-Disposition: attachment; filename=\"" . $dadosArquivo['uploadNomeOriginal'] . "\";");
+            \header("Content-Transfer-Encoding: binary");
+            \header("Content-Length: " . filesize($arquivo));
+        
+            \readfile("$arquivo") or die("Arquivo não encontrado.");
+        }
+
+    } catch(Exception $e){
+        exit($e->getMessage());
     }
-
-    \set_time_limit(0);
-
-    \header("Pragma: public");
-    \header("Expires: 0");
-    \header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-    \header("Cache-Control: private", false);
-    //header("Content-Type:".mime_content_type($arquivo)."");
-    \header("Content-Disposition: attachment; filename=\"" . $dadosArquivo['uploadNomeOriginal'] . "\";");
-    \header("Content-Transfer-Encoding: binary");
-    \header("Content-Length: " . filesize($arquivo));
-
-    \readfile("$arquivo") or die("Arquivo não encontrado.");
-}
